0a8cbb0f94a73ab1073b21a0419bac6d8e51918a
jonbiveo <logantedmonds@gmail.com>
2022-08-24 12:05:29 -0500

    updated recursive runtime note

--- chapter-notes/recursive-runtime-pg44.js
+++ chapter-notes/recursive-runtime-pg44.js
@@ -2,9 +2,6 @@
  * We are looking at this function f that takes in an input of int n and returns an int.
  * This function will recursively return f(n-1) + f(n-1) until it hits
  * base case of n <= 1.
- * 
- * 
- * 
  */
 
 const f = function(n) {
@@ -15,7 +12,15 @@ const f = function(n) {
 }
 //returns a number
 
-//console.log(f(4));
+console.log(f(4));
+//             f4
+//           f3 + f3
+//       f2 + f2    f2 + f2
+// 1 + 1  1 + 1  1 + 1  1 + 1
+
+// Nodes at max depth is 2^(n-1)
+// Total nodes is 2^(n+1) - 1. Expanded out =  2^n + 2^1 - 1 
+// The exponent n+1 accounts for the zero index of the binary tree. The -1 accounts for the first level (level 0) having only one node.
 
 // Q1. Are there integers in JavaScript?
 // Simply no, there is only number type. There is BigInt, but is not compatible with number. Number itself can handle math up to the integer value limits. 
@@ -34,5 +39,5 @@ const f = function(n) {
 // Symbol type
 // Objects (collections of properties)
 
-console.log(Number.MAX_VALUE * 1.000000000000001)
+// console.log(Number.MAX_VALUE * 1.000000000000001)
 // anything above Number.MAX_VALUE returns Infinity
